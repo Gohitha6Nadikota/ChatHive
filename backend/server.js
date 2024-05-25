0e37a13e2f63c8af9547a48e4cf88b00b0556c82
Gohitha6Nadikota <gohithapriyanadikota@gmail.com>
2024-05-25 16:17:54 +0530

    Socket Port Change

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ const server=app.listen(PORT,console.log(`Server started at ${PORT}`));
 const io=require('socket.io')(server,{
     pingTimeout:60000,
     cors:{
-        origin:'http://localhost:3000'
+        origin:'http://localhost:4000'
     }
 })
 io.on("connection",(socket)=>{
